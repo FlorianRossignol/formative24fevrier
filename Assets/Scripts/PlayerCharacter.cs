63aeaa2a2fe8834fe7c19b733a7448c4bfb37016
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-02-22 14:04:07 +0100

    adding prefabs playercharacter and script for it

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer Playecharactersprite_;
+    [SerializeField] private Rigidbody2D Bodyplayer_;
+    private Transform Playertransform_;
+    private const float MoveSpeed = 2.0f;
+    void Start()
+    {
+        Playertransform_ = GetComponent < Transform>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        Bodyplayer_.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed, Bodyplayer_.velocity.y);
+        Bodyplayer_.velocity = new Vector2(Bodyplayer_.velocity.x, Input.GetAxis("Vertical") * MoveSpeed);
+    }
+
+    void Update()
+    {
+        
+    }
+}
